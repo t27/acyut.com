5e59f2cd4bdbc86525062997404fd39569673d6f
tarang <tarang27@gmail.com>
2017-03-22 13:02:11 +0530

    Updated site and adding to GIT

---
+++ assets/js/pages/page_contacts.js
@@ -0,0 +1,37 @@
+var ContactPage = function () {
+
+    return {
+        
+    	//Basic Map
+        initMap: function () {
+			var map;
+			$(document).ready(function(){
+			  map = new GMaps({
+				div: '#map',
+				scrollwheel: false,				
+				lat: 28.36446,
+				lng: 75.58805
+			  });
+			  
+			  var marker = map.addMarker({
+				lat: 28.36446,
+				lng: 75.58805,
+	            title: 'CRIS, BITS Pilani'
+		       });
+			});
+        },
+
+        //Panorama Map
+        initPanorama: function () {
+		    var panorama;
+		    $(document).ready(function(){
+		      panorama = GMaps.createPanorama({
+		        el: '#panorama',
+		        lat : 28.36446,
+		        lng : 75.58805
+		      });
+		    });
+		}        
+
+    };
+}();
---
+++ webpages/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+
+	<meta http-equiv="refresh" content="0; url=http://www.acyut.com" />
+
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="google-site-verification" content="3WCWM6dbp5x1MldYIA-MADjslduWc4-3xn7g43CCw7M" />
+<meta charset="UTF-8" />
+<meta name="google" content="notranslate">
+<meta http-equiv="Content-Language" content="en" />	
+    <!--	Including Required Files	-->    
+    
+    <title>Team AcYut : Redirecting</title>
+    
+</head>
+
+<body onload="loader();">
+	<a href="http://www.acyut.com">Redirect</a>
+</body>
+</html>
